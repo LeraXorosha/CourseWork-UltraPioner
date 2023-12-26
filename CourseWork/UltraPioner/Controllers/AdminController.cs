a5a2b117f038c10866f437a2b8c473af44668bb9
Valeri <choroshunovalera678@gmail.com>
2023-12-27 05:25:21 +0700

    adminTable

--- CourseWork/UltraPioner/Controllers/AdminController.cs
+++ CourseWork/UltraPioner/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UltraPioner.Models;
+using UltraPioner.Models.ViewModels;
 
 namespace UltraPioner.Controllers
 {
@@ -16,7 +17,39 @@ namespace UltraPioner.Controllers
 
 		public async Task<IActionResult> Index()
 		{
-			return View();
+			var users = from user in _db.PersonalDatas
+					   join role in _db.Roles on user.RoleId equals role.Id
+					   orderby user.Name descending
+
+
+					   select new
+					   {
+						   UserName = user.Name,
+						   UserRole = role.RoleName,
+						   UserDiscription = user.Discription,
+						   UserLogin = user.Login,
+						   UserPassword = user.Password,
+						   UserPhone = user.Phone,
+						   UserEmail = user.Email
+
+					   };
+
+
+			var anyUsers = users.ToList()
+				.Select(g => new AdminTableModel
+				{
+					Name = g.UserName,
+					RoleName = g.UserRole,
+					Discription= g.UserDiscription,
+					Login = g.UserLogin,
+					Password = g.UserPassword,
+					Phone = g.UserPhone,
+					Email = g.UserEmail
+
+				})
+				.ToList();
+
+			return View(anyUsers);
 		}
 	}
 }
---
+++ CourseWork/UltraPioner/Models/ViewModels/AdminTableModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UltraPioner.Models.ViewModels
+{
+	public class AdminTableModel
+	{
+		public string Name { get; set; }
+	
+		public string Discription { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+
+		public string Login { get; set; }
+		public string Password { get; set; }
+		public string RoleName { get; set; }
+	}
+}
